486b75f4862320b71131dd028f5b85dd2debd950
John W. Schmotzer <jwschmo@users.noreply.github.com>
2025-05-19 15:34:25 -0400

    Update get_repo.sh
    VOID_VERSION -> AUTOIDE_VERSION

--- get_repo.sh
+++ get_repo.sh
@@ -29,34 +29,34 @@ mkdir -p vscode
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
 
 git init -q
-git remote add origin https://github.com/voideditor/void.git
+git remote add origin https://github.com/NexusIQPlatform/autoIDE.git
 
 # Allow callers to specify a particular commit to checkout via the
-# environment variable VOID_COMMIT.  We still default to the tip of the
-# ${VOID_BRANCH} branch when the variable is not provided.  Keeping
-# VOID_BRANCH as "main" ensures the rest of the script (and downstream
+# environment variable AUTOIDE_COMMIT.  We still default to the tip of the
+# ${AUTOIDE_BRANCH} branch when the variable is not provided.  Keeping
+# AUTOIDE_BRANCH as "main" ensures the rest of the script (and downstream
 # consumers) behave exactly as before.
-if [[ -n "${VOID_COMMIT}" ]]; then
-  echo "Using explicit commit ${VOID_COMMIT}"
+if [[ -n "${AUTOIDE_COMMIT}" ]]; then
+  echo "Using explicit commit ${AUTOIDE_COMMIT}"
   # Fetch just that commit to keep the clone shallow.
-  git fetch --depth 1 origin "${VOID_COMMIT}"
-  git checkout "${VOID_COMMIT}"
+  git fetch --depth 1 origin "${AUTOIDE_COMMIT}"
+  git checkout "${AUTOIDE_COMMIT}"
 else
-  git fetch --depth 1 origin "${VOID_BRANCH}"
+  git fetch --depth 1 origin "${AUTOIDE_BRANCH}"
   git checkout FETCH_HEAD
 fi
 
 MS_TAG=$( jq -r '.version' "package.json" )
-MS_COMMIT=$VOID_BRANCH # Void - MS_COMMIT doesn't seem to do much
-VOID_VERSION=$( jq -r '.voidVersion' "product.json" ) # Void added this
+MS_COMMIT=$AUTOIDE_BRANCH # autoIDE - MS_COMMIT doesn't seem to do much
+AUTOIDE_VERSION=$( jq -r '.autoideVersion' "product.json" ) # Void added this
 
-if [[ -n "${VOID_RELEASE}" ]]; then # Void added VOID_RELEASE as optional to bump manually
-  RELEASE_VERSION="${MS_TAG}${VOID_RELEASE}"
+if [[ -n "${AUTOIDE_RELEASE}" ]]; then # AutoIDE added AUTOIDE_RELEASE as optional to bump manually
+  RELEASE_VERSION="${MS_TAG}${AUTOIDE_RELEASE}"
 else
-  VOID_RELEASE=$( jq -r '.voidRelease' "product.json" )
-  RELEASE_VERSION="${MS_TAG}${VOID_RELEASE}"
+  AUTOIDE_RELEASE=$( jq -r '.autoideRelease' "product.json" )
+  RELEASE_VERSION="${MS_TAG}${AUTOIDE_RELEASE}"
 fi
-# Void - RELEASE_VERSION is later used as version (1.0.3+RELEASE_VERSION), so it MUST be a number or it will throw a semver error in void
+# AutoIDE - RELEASE_VERSION is later used as version (1.0.3+RELEASE_VERSION), so it MUST be a number or it will throw a semver error in void
 
 
 echo "RELEASE_VERSION=\"${RELEASE_VERSION}\""
@@ -70,7 +70,7 @@ if [[ "${GITHUB_ENV}" ]]; then
   echo "MS_TAG=${MS_TAG}" >> "${GITHUB_ENV}"
   echo "MS_COMMIT=${MS_COMMIT}" >> "${GITHUB_ENV}"
   echo "RELEASE_VERSION=${RELEASE_VERSION}" >> "${GITHUB_ENV}"
-  echo "VOID_VERSION=${VOID_VERSION}" >> "${GITHUB_ENV}" # Void added this
+  echo "VOID_VERSION=${AUTOIDE_VERSION}" >> "${GITHUB_ENV}" # Void added this
 fi
 
 
@@ -79,11 +79,11 @@ echo "----------- get_repo exports -----------"
 echo "MS_TAG ${MS_TAG}"
 echo "MS_COMMIT ${MS_COMMIT}"
 echo "RELEASE_VERSION ${RELEASE_VERSION}"
-echo "VOID VERSION ${VOID_VERSION}"
+echo "AUTOIDE VERSION ${AUTOIDE_VERSION}"
 echo "----------------------"
 
 
 export MS_TAG
 export MS_COMMIT
 export RELEASE_VERSION
-export VOID_VERSION
+export AUTOIDE_VERSION
