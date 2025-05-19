519f13bcf96aa348248adb1cb7b94500734ee191
John W. Schmotzer <jwschmo@users.noreply.github.com>
2025-05-19 15:21:17 -0400

    Update build.sh

--- dev/build.sh
+++ dev/build.sh
@@ -5,11 +5,11 @@
 # to run with Bash: "C:\Program Files\Git\bin\bash.exe" ./dev/build.sh
 ###
 
-export APP_NAME="Void"
+export APP_NAME="OlympusIDE"
 export BINARY_NAME="codium"
 export CI_BUILD="no"
-export GH_REPO_PATH="voideditor/void"
-export ORG_NAME="voideditor"
+export GH_REPO_PATH="NexusIQPlatform/autoIDE"
+export ORG_NAME="NexusIQPlatform"
 export SHOULD_BUILD="yes"
 export SKIP_ASSETS="yes"
 export SKIP_BUILD="no"
