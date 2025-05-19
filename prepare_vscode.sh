8f3974933c092330afa5c25c6f102bcb4dbae4ea
John W. Schmotzer <jwschmo@users.noreply.github.com>
2025-05-19 15:29:50 -0400

    Update prepare_vscode.sh

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -123,42 +123,42 @@ setpath_json() {
 cp product.json{,.bak}
 
 setpath "product" "checksumFailMoreInfoUrl" "https://go.microsoft.com/fwlink/?LinkId=828886"
-setpath "product" "documentationUrl" "https://voideditor.com"
+setpath "product" "documentationUrl" "https://olympianmotors.com/olympus-os"
 # setpath_json "product" "extensionsGallery" '{"serviceUrl": "https://open-vsx.org/vscode/gallery", "itemUrl": "https://open-vsx.org/vscode/item"}'
 setpath "product" "introductoryVideosUrl" "https://go.microsoft.com/fwlink/?linkid=832146"
 setpath "product" "keyboardShortcutsUrlLinux" "https://go.microsoft.com/fwlink/?linkid=832144"
 setpath "product" "keyboardShortcutsUrlMac" "https://go.microsoft.com/fwlink/?linkid=832143"
 setpath "product" "keyboardShortcutsUrlWin" "https://go.microsoft.com/fwlink/?linkid=832145"
-setpath "product" "licenseUrl" "https://github.com/voideditor/void/blob/main/LICENSE.txt"
+setpath "product" "licenseUrl" "https://github.com/NexusIQPlatform/autoIDE/blob/main/LICENSE.txt"
 # setpath_json "product" "linkProtectionTrustedDomains" '["https://open-vsx.org"]'
 # setpath "product" "releaseNotesUrl" "https://go.microsoft.com/fwlink/?LinkID=533483#vscode"
-setpath "product" "reportIssueUrl" "https://github.com/voideditor/void/issues/new"
-setpath "product" "requestFeatureUrl" "https://github.com/voideditor/void/issues/new"
+setpath "product" "reportIssueUrl" "https://github.com/NexusIQPlatform/autoIDE/issues/new"
+setpath "product" "requestFeatureUrl" "https://github.com/NexusIQPlatform/autoIDE/issues/new"
 setpath "product" "tipsAndTricksUrl" "https://go.microsoft.com/fwlink/?linkid=852118"
-setpath "product" "twitterUrl" "https://x.com/thevoideditor"
+setpath "product" "twitterUrl" "https://x.com/olympianmotors"
 
 if [[ "${DISABLE_UPDATE}" != "yes" ]]; then
-  setpath "product" "updateUrl" "https://raw.githubusercontent.com/voideditor/versions/refs/heads/main"
-  setpath "product" "downloadUrl" "https://github.com/voideditor/binaries/releases"
+  setpath "product" "updateUrl" "https://raw.githubusercontent.com/NexusIQPlatform/versions/refs/heads/main"
+  setpath "product" "downloadUrl" "https://github.com/NexusIQPlatform/binaries/releases"
 fi
 
 if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-  setpath "product" "nameShort" "Void - Insiders"
-  setpath "product" "nameLong" "Void - Insiders"
-  setpath "product" "applicationName" "void-insiders"
-  setpath "product" "dataFolderName" ".void-insiders"
-  setpath "product" "linuxIconName" "void-insiders"
+  setpath "product" "nameShort" "autoIDE - Insiders"
+  setpath "product" "nameLong" "autoIDE - Insiders"
+  setpath "product" "applicationName" "autoide-insiders"
+  setpath "product" "dataFolderName" ".autoide-insiders"
+  setpath "product" "linuxIconName" "autoide-insiders"
   setpath "product" "quality" "insider"
-  setpath "product" "urlProtocol" "void-insiders"
-  setpath "product" "serverApplicationName" "void-server-insiders"
-  setpath "product" "serverDataFolderName" ".void-server-insiders"
-  setpath "product" "darwinBundleIdentifier" "ai.voideditor.VoidInsiders"
-  setpath "product" "win32AppUserModelId" "Void.VoidInsiders"
-  setpath "product" "win32DirName" "Void Insiders"
-  setpath "product" "win32MutexName" "voidinsiders"
-  setpath "product" "win32NameVersion" "Void Insiders"
-  setpath "product" "win32RegValueName" "VoidInsiders"
-  setpath "product" "win32ShellNameShort" "Void Insiders"
+  setpath "product" "urlProtocol" "autoide-insiders"
+  setpath "product" "serverApplicationName" "autoide-server-insiders"
+  setpath "product" "serverDataFolderName" ".autoide-server-insiders"
+  setpath "product" "darwinBundleIdentifier" "ai.NexusIQPlatform.autoIDEInsiders"
+  setpath "product" "win32AppUserModelId" "AutoIDE.AutoIDEInsiders"
+  setpath "product" "win32DirName" "AutoIDE Insiders"
+  setpath "product" "win32MutexName" "autoideinsiders"
+  setpath "product" "win32NameVersion" "AutoIDE Insiders"
+  setpath "product" "win32RegValueName" "AutoIDEInsiders"
+  setpath "product" "win32ShellNameShort" "AutoIDE Insiders"
   setpath "product" "win32AppId" "{{5893CE20-77AA-4856-A655-ECE65CBCF1C7}"
   setpath "product" "win32x64AppId" "{{7A261980-5847-44B6-B554-31DF0F5CDFC9}"
   setpath "product" "win32arm64AppId" "{{EE4FF7AA-A874-419D-BAE0-168C9DBCE211}"
@@ -166,22 +166,22 @@ if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
   setpath "product" "win32x64UserAppId" "{{5B1813E3-1D97-4E00-AF59-C59A39CF066A}"
   setpath "product" "win32arm64UserAppId" "{{C2FA90D8-B265-41B1-B909-3BAEB21CAA9D}"
 else
-  setpath "product" "nameShort" "Void"
-  setpath "product" "nameLong" "Void"
-  setpath "product" "applicationName" "void"
-  setpath "product" "linuxIconName" "void"
+  setpath "product" "nameShort" "AutoIDE"
+  setpath "product" "nameLong" "AutoIDE"
+  setpath "product" "applicationName" "autoide"
+  setpath "product" "linuxIconName" "autoide"
   setpath "product" "quality" "stable"
-  setpath "product" "urlProtocol" "void"
-  setpath "product" "serverApplicationName" "void-server"
-  setpath "product" "serverDataFolderName" ".void-server"
-  setpath "product" "darwinBundleIdentifier" "com.void"
-  setpath "product" "win32AppUserModelId" "Void.Void"
-  setpath "product" "win32DirName" "Void"
-  setpath "product" "win32MutexName" "void"
-  setpath "product" "win32NameVersion" "Void"
-  setpath "product" "win32RegValueName" "Void"
-  setpath "product" "win32ShellNameShort" "Void"
-  # Void - already set in product
+  setpath "product" "urlProtocol" "autoide"
+  setpath "product" "serverApplicationName" "autoide-server"
+  setpath "product" "serverDataFolderName" ".autoide-server"
+  setpath "product" "darwinBundleIdentifier" "com.autoide"
+  setpath "product" "win32AppUserModelId" "AutoIDE.AutoIDE"
+  setpath "product" "win32DirName" "AutoIDE"
+  setpath "product" "win32MutexName" "AutoIDE"
+  setpath "product" "win32NameVersion" "AutoIDE"
+  setpath "product" "win32RegValueName" "AutoIDE"
+  setpath "product" "win32ShellNameShort" "AutoIDE"
+  # AutoIDE - already set in product
   # setpath "product" "win32AppId" "{{88DA3577-054F-4CA1-8122-7D820494CFFB}"
   # setpath "product" "win32x64AppId" "{{9D394D01-1728-45A7-B997-A6C82C5452C3}"
   # setpath "product" "win32arm64AppId" "{{0668DD58-2BDE-4101-8CDA-40252DF8875D}"
@@ -205,22 +205,22 @@ replace 's|Microsoft Corporation|Void|' package.json
 cp resources/server/manifest.json{,.bak}
 
 if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-  setpath "resources/server/manifest" "name" "Void - Insiders"
-  setpath "resources/server/manifest" "short_name" "Void - Insiders"
+  setpath "resources/server/manifest" "name" "AutoIDE - Insiders"
+  setpath "resources/server/manifest" "short_name" "AutoIDE - Insiders"
 else
-  setpath "resources/server/manifest" "name" "Void"
-  setpath "resources/server/manifest" "short_name" "Void"
+  setpath "resources/server/manifest" "name" "AutoIDE"
+  setpath "resources/server/manifest" "short_name" "AutoIDE"
 fi
 
 cp resources/server/manifest.json{,.bak}
 
 if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-  setpath "resources/server/manifest" "name" "Void - Insiders"
-  setpath "resources/server/manifest" "short_name" "Void - Insiders"
+  setpath "resources/server/manifest" "name" "AutoIDE - Insiders"
+  setpath "resources/server/manifest" "short_name" "AutoIDE - Insiders"
 else
-  # Void already has this
-  setpath "resources/server/manifest" "name" "Void"
-  setpath "resources/server/manifest" "short_name" "Void"
+  # AutoIDE already has this
+  setpath "resources/server/manifest" "name" "AutoIDE"
+  setpath "resources/server/manifest" "short_name" "AutoIDE"
 fi
 
 # announcements
@@ -228,10 +228,10 @@ fi
 
 ../undo_telemetry.sh
 
-replace 's|Microsoft Corporation|Void|' build/lib/electron.js
-replace 's|Microsoft Corporation|Void|' build/lib/electron.ts
-replace 's|([0-9]) Microsoft|\1 Void|' build/lib/electron.js
-replace 's|([0-9]) Microsoft|\1 Void|' build/lib/electron.ts
+replace 's|Microsoft Corporation|AutoIDE|' build/lib/electron.js
+replace 's|Microsoft Corporation|AutoIDE|' build/lib/electron.ts
+replace 's|([0-9]) Microsoft|\1 AutoIDE|' build/lib/electron.js
+replace 's|([0-9]) Microsoft|\1 AutoIDE|' build/lib/electron.ts
 
 if [[ "${OS_NAME}" == "linux" ]]; then
   # microsoft adds their apt repo to sources
@@ -247,28 +247,28 @@ if [[ "${OS_NAME}" == "linux" ]]; then
   # fix the packages metadata
   # code.appdata.xml
   sed -i 's|Visual Studio Code|Void|g' resources/linux/code.appdata.xml
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://voideditor.com|' resources/linux/code.appdata.xml
+  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://NexusIQPlatform.com|' resources/linux/code.appdata.xml
   sed -i 's|https://code.visualstudio.com/home/home-screenshot-linux-lg.png|https://vscodium.com/img/vscodium.png|' resources/linux/code.appdata.xml
-  sed -i 's|https://code.visualstudio.com|https://voideditor.com|' resources/linux/code.appdata.xml
+  sed -i 's|https://code.visualstudio.com|https://NexusIQPlatform.com|' resources/linux/code.appdata.xml
 
   # control.template
-  sed -i 's|Microsoft Corporation <vscode-linux@microsoft.com>|Void Team <team@voideditor.com>|'  resources/linux/debian/control.template
+  sed -i 's|Microsoft Corporation <vscode-linux@microsoft.com>|OlympusOS Team <team@NexusIQPlatform.com>|'  resources/linux/debian/control.template
   sed -i 's|Visual Studio Code|Void|g' resources/linux/debian/control.template
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://voideditor.com|' resources/linux/debian/control.template
-  sed -i 's|https://code.visualstudio.com|https://voideditor.com|' resources/linux/debian/control.template
+  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://NexusIQPlatform.com|' resources/linux/debian/control.template
+  sed -i 's|https://code.visualstudio.com|https://NexusIQPlatform.com|' resources/linux/debian/control.template
 
   # code.spec.template
   sed -i 's|Microsoft Corporation|Void Team|' resources/linux/rpm/code.spec.template
-  sed -i 's|Visual Studio Code Team <vscode-linux@microsoft.com>|Void Team <team@voideditor.com>|' resources/linux/rpm/code.spec.template
+  sed -i 's|Visual Studio Code Team <vscode-linux@microsoft.com>|OlympusOS Team <team@NexusIQPlatform.com>|' resources/linux/rpm/code.spec.template
   sed -i 's|Visual Studio Code|Void|' resources/linux/rpm/code.spec.template
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://voideditor.com|' resources/linux/rpm/code.spec.template
-  sed -i 's|https://code.visualstudio.com|https://voideditor.com|' resources/linux/rpm/code.spec.template
+  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://NexusIQPlatform.com|' resources/linux/rpm/code.spec.template
+  sed -i 's|https://code.visualstudio.com|https://NexusIQPlatform.com|' resources/linux/rpm/code.spec.template
 
   # snapcraft.yaml
   sed -i 's|Visual Studio Code|Void|'  resources/linux/rpm/code.spec.template
 elif [[ "${OS_NAME}" == "windows" ]]; then
   # code.iss
-  sed -i 's|https://code.visualstudio.com|https://voideditor.com|' build/win32/code.iss
+  sed -i 's|https://code.visualstudio.com|https://NexusIQPlatform.com|' build/win32/code.iss
   sed -i 's|Microsoft Corporation|Void|' build/win32/code.iss
 fi
 
